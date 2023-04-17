16e70b7051ed6dbfe7576d6447b2eebc40949bc2
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-04-18 01:02:58 +0300

    Add event listener to hamburger and style the mobile menu window

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const mobileMenu = document.querySelector("#menu-icon")
 mobileMenu.addEventListener("click", show)
 const mobileNav = document.querySelector("#desk-nav")
-
+const logo = document.querySelector("#logo")
 function show(){
     mobileNav.style.display = "flex";
     mobileNav.style.top = "0"
-    mobileNav.style.height = "100vh"  
+    mobileNav.style.height = "100vh" 
+    mobileMenu.style.display = "none"; 
+    logo.style.display = "none"
 }
