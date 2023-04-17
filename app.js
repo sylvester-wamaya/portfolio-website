0e4791ecaf600c160c05b5ff1a327cf016cad5e0
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-04-18 01:47:47 +0300

    Add Javascript linters and fix linter errors

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const mobileMenu = document.querySelector("#menu-icon")
-const mobileNav = document.querySelector("#desk-nav")
-const logo = document.querySelector("#logo")
-const closeIcon = document.querySelector("#close img")
-const menuList = document.querySelectorAll(".desk-nav")
-mobileMenu.addEventListener("click", show)
-closeIcon.addEventListener("click", close)
-menuList.forEach(nav => nav.addEventListener("click",close))
-function show(){
-    mobileNav.style.display = "flex";   
-    mobileMenu.style.display = "none"; 
-    logo.style.display = "none"
+const mobileMenu = document.querySelector('#menu-icon');
+const mobileNav = document.querySelector('#desk-nav');
+const logo = document.querySelector('#logo');
+const closeIcon = document.querySelector('#close img');
+const menuList = document.querySelectorAll('.desk-nav');
+
+function show() {
+  mobileNav.style.display = 'flex';
+  mobileMenu.style.display = 'none';
+  logo.style.display = 'none';
 }
-function close(){
-    logo.style.display = ""
-    mobileMenu.style.display = "";     
-    mobileNav.style.display= "none"
+
+function close() {
+  logo.style.display = '';
+  mobileMenu.style.display = '';
+  mobileNav.style.display = 'none';
 }
+
+mobileMenu.addEventListener('click', show);
+closeIcon.addEventListener('click', close);
+menuList.forEach((nav) => nav.addEventListener('click', close));
