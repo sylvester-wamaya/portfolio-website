098ec57e44f3429036856271e43610bb3d2f73c4
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-04-18 23:52:21 +0300

    Add project data in javascript object

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const logo = document.querySelector('#logo');
 const closeIcon = document.querySelector('#close img');
 const menuList = document.querySelectorAll('.desk-nav');
 
+
+const projList = document.querySelectorAll
+
 function show() {
   mobileNav.style.display = 'flex';
   mobileMenu.style.display = 'none';
@@ -19,3 +22,71 @@ function close() {
 mobileMenu.addEventListener('click', show);
 closeIcon.addEventListener('click', close);
 menuList.forEach((nav) => nav.addEventListener('click', close));
+
+
+
+const projects = [
+ {
+    id: 1,
+    name: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: "media/Img Placeholder.png",
+    technologies: ["CSS", "HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 2,
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 3,
+    name: "Data Dashboard Healthcare",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 4,
+    name: "Website Portfolio",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 5,
+    name: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 6,
+    name: "Data Dashboard Healthcare",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ },
+ {
+    id: 7,
+    name: "Website Portfolio",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    image: "media/Img Placeholder.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"] ,
+    liveLink: "https://sylvester-wamaya.github.io/portfolio-website/",
+    sourceLink: "https://github.com/sylvester-wamaya/portfolio-website"
+ }
+]
